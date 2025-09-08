7e6048d3b97537bd5512a96fbc6b6fd8825f1c99
admin <techtalktoyesh@gmail.com>
2025-09-08 23:03:35 +0530

    changes

--- src/app/components/tutorial-details/tutorial-details.component.ts
+++ src/app/components/tutorial-details/tutorial-details.component.ts
@@ -62,7 +62,7 @@ export class TutorialDetailsComponent implements OnInit {
 
   updateTutorial(): void {
     this.message = '';
-
+//test
     this.tutorialService
       .update(this.currentTutorial.id, this.currentTutorial)
       .subscribe({
